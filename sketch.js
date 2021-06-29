a52ebc339e67a47e1f11293ff8560f91ed4c7107
Shravu2108 <74601670+Shravu2108@users.noreply.github.com>
2021-06-29 17:36:12 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,44 +1,52 @@
 
-var blue,blue_Img;
+var ground, groundImg;
+var suga,suga_Img;
 var which;
 var count=0;
 
 function preload(){
- blue_Img = loadImage("images/blue.jpg");
- suga_Img = loadImage("images/suga.png");
- bg_Img = loadImage("images/bg.jpg");
+ blue_Img = loadAnimation("images/blue.jpg");
+ suga_Img = loadAnimation("images/suga.png");
+ groundImg = loadImage("images/bg.jpg");
 }
 
 function setup(){
-  createCanvas(500,500);
- 
+  createCanvas(1000,600);
+  ground = createSprite(0,0,500,500);
+  ground.addImage(groundImg);
+  ground.scale = 7;
+  ground.velocityY = 6;
 }
 function draw() {
   background("white");
+  if(ground.y>500){
+    ground.y = ground.height/2;
+  }
   spawnTiles();
   drawSprites();
   textSize(15)
   fill("black")
   text("score="+count, 0, 15);  
-  if (mousePressedOver(blue) && which==1) 
+  if (mousePressedOver(suga) && which==1) 
   {
     count=count+1;
-    blue.destroy();
+    //suga.destroy();
+    suga.changeAnimation("suga1",blue_Img);
     
   }
-  if (mousePressedOver(blue) && which==2){
+  if (mousePressedOver(suga) && which==2){
     count=count-1;
-    blue.destroy();
+    //suga.destroy();
   }
 }
 
 function spawnTiles(){
   if(frameCount%150===0){
   which=Math.round(random(1,2));
-  blue = createSprite(200, 0);
-  blue.velocityY=8;
-  blue.addImage("blue",blue_Img);
-  blue.scale=0.2;
+  suga = createSprite(random(200,800), 0);
+  suga.velocityY=8;
+  suga.addAnimation("suga1",suga_Img);
+  suga.scale=0.1;
     
   }
   
