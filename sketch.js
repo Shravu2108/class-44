56e9d775a8934580f587d98822c8ac8b8544bc51
Shravu2108 <74601670+Shravu2108@users.noreply.github.com>
2021-07-02 17:43:13 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,15 +1,17 @@
 
 var ground, groundImg;
-var suga,suga_Img;
+var suga,suga_Img, sugaGroup;
 var which;
 var count=0;
-
+var sound;
+var gameState = "play";
 
 function preload(){
  blue_Img = loadImage("images/blue.jpg");
  suga_Img = loadImage("images/suga.png");
  groundImg = loadImage("images/bg.jpg");
-
+ sound = loadSound("music/sound.mp3");
+ 
 }
 
 function setup(){
@@ -18,34 +20,53 @@ function setup(){
   ground.addImage(groundImg);
   ground.scale = 7;
   ground.velocityY = 6;
+
+  sugaGroup = new Group();
 }
 function draw() {
   background("white");
+  stroke("white")
+  textSize(30)
+  fill("black")
+  text("Score="+count, 820, 45); 
+  sound.play();
+  if(gameState === "play"){
+
   
   if(ground.y>500){
     ground.y = ground.height/2;
   }
   spawnTiles();
   drawSprites();
-  stroke("white")
-  textSize(30)
-  fill("black")
-  text("Score="+count, 820, 45);  
+   
   if (mousePressedOver(suga)) 
   {
    count=count+1; 
    suga.changeImage("blue1");
    suga.scale=0.5;
   }
+  if(suga.y>600){
+    gameState = "end";
+  }
+}
+else if(gameState === "end"){
+  ground.velocityY = 0;
+  sugaGroup.setVelocityYEach(0);
+  sugaGroup.setLifetimeEach(-1);
+}
+
 }
 
 function spawnTiles(){
   if(frameCount%150===0){
   which=Math.round(random(1,2));
   suga = createSprite(random(200,800), 0);
-  suga.velocityY=8;
+  suga.velocityY=15;
   suga.addImage("suga1",suga_Img);
   suga.addImage("blue1",blue_Img);
   suga.scale=0.1;
+  suga.lifetime = 600;
+  sugaGroup.add(suga);
+  
   } 
 }
